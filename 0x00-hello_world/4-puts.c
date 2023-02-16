d2a2ba706d1e65412c606cf4ff38431e878797d4
nuublx <zeyaddiaa21@gmail.com>
2023-02-16 13:04:44 -0800

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-/*
- * main - this function prints a statement using puts function.
+/**
+ * main - Entry point
+ *
+ * Description: 'prints a statement using puts function.'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
