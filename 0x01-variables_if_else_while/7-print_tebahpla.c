5bf25939b6f5188e451c151c5af637c0d2b27d1f
nuublx <zeyaddiaa21@gmail.com>
2023-02-17 18:00:33 -0800

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: prints a message that depends on
+ * last digit of the random number generated
+ * Return: Always 0 Success
+*/
+int main(void)
+{
+	char message[] = "abcdfghijklmnoprstuvwxyz";
+	int count;
+
+	for (count = 23; count >= 0; count--)
+	{
+		putchar(message[count]);
+	}
+	putchar('\n');
+	return (0);
+}
