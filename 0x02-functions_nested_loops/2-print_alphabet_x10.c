dd83bb90072d526877faab9d20b4641aed0b2c7e
nuublx <zeyaddiaa21@gmail.com>
2023-02-21 05:58:08 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+void print_alphabet_x10(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for(j = 0; j < 26; j++)
+		{
+			_puttchar(i + 'a');
+		}
+		_putchar('\n');
+	}
+}
