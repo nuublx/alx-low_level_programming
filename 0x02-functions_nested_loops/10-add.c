126a2b9c6386437beac0b1bac39a444b73bc440a
nuublx <zeyaddiaa21@gmail.com>
2023-02-21 11:30:54 -0800

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,6 +4,9 @@
  * Description: adds two integers and returns the result
  *
  * Return: an integer
+ * @a: integer number 1 to be summed
+ *
+ * @b: integer number 2 to be summed
 */
 int add(int a, int b)
 {
