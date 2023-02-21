c195cd15b8384e077405e11b9b00e82e1dd6944e
nuublx <zeyaddiaa21@gmail.com>
2023-02-21 05:50:27 -0800

    task 1 update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 #include"main.h"
+/**
+ * print_alphabet - void parameters
+ * Description: prints alphabet in small letters
+ * Return: void Always
+*/
 void print_alphabet(void)
 {
 	int i;
