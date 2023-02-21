3a74021a39f49e8a71cfec9435f9e6f6f7d3304e
nuublx <zeyaddiaa21@gmail.com>
2023-02-21 10:36:04 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+ * jack_bauer - prints the day in minutes
+ *
+ * Description: prints from 00:00 to 23:59
+ *
+ * Return: void Always
+*/
+void jack_bauer(void)
+{
+	int hours1, hours2, minutes1, minutes2;
+
+	for (hours1 = 0; hours1 < 3; hours1++)
+	{
+		for (hours2 = 0; hours2 < 4; hours2++)
+		{
+			for (minutes1 = 0; minutes1 < 6; minutes1++)
+			{
+				for (minutes2 = 0; minutes2 < 10; minutes2++)
+				{
+					_putchar(hours1 + '0');
+					_putchar(hours2 + '0');
+					_putchar(':');
+					_putchar(minutes1 + '0');
+					_putchar(minutes2 + '0');
+					_putchar('\n');
+				}
+
+			}
+		}
+	}
+}
