012cce9817fcd3c95ee6f6b43d6b16ed050f149a
nuublx <zeyaddiaa21@gmail.com>
2023-02-27 13:06:22 -0800

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - prints an integer array
+ * @a: integer array
+ * @n: size of array
+ *
+ * Return: void ALWAYS
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[i]);
+}
