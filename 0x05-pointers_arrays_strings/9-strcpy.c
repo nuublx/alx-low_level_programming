43830b92a0ea8345848aa326125c90a13ea3ee5b
nuublx <zeyaddiaa21@gmail.com>
2023-02-27 13:18:34 -0800

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+/**
+ * _strcpy - copies from src to dest
+ * @src: string that will copy from
+ * @dest: string to copy into
+ *
+ * Return: string dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
