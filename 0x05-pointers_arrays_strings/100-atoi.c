2e90b545b9fd738a8ada16a8e33c17751dc8de3c
nuublx <zeyaddiaa21@gmail.com>
2023-02-27 14:41:19 -0800

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,6 +16,11 @@ int _atoi(char *s)
 
 	while (i > 0 && s[i] >= '0' && s[i] <= '9')
 	{
+		if (s[i] == ' ')
+		{
+			i--;
+			continue;
+		}
 		sum += (s[i] - '0') * digit;
 		digit *= 10;
 		i--;
@@ -26,7 +31,7 @@ int _atoi(char *s)
 	else if (s[i] != '+' && s[i] >= '0' && s[i] <= '9')
 		sum += (s[i] - '0') * digit;
 
-	else if (s[i] < '0' || s[i] > '9')
+	else if ((s[i] < '0' || s[i] > '9') && s[i] != ' ')
 		return (0); 
 	return (sum);
 
