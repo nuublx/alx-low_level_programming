dbe1da29069c97da941076f1e8da4079b9f7a463
nuublx <zeyaddiaa21@gmail.com>
2023-02-27 12:01:58 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,13 @@ void puts2(char *str)
 	int len, i;
 
 	len = 0;
-	
+
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 
-	for (i = 0; i < len; i+=2)
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
