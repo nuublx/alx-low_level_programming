6d56253fb0f83174f29b331b26f0ea5d5a114a58
nuublx <zeyaddiaa21@gmail.com>
2023-03-01 04:28:21 -0800

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+/**
+ * _strcmp - compares between two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: -15 if s1 < s2, 15 s1 > s2, 0 s1 = s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, sum1, sum2;
+
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
