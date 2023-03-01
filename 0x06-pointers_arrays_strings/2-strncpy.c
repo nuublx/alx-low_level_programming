43c8420b151cb783549e44b7d194ad88b3c3f8ce
nuublx <zeyaddiaa21@gmail.com>
2023-03-01 03:56:11 -0800

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+/**
+ * _strncpy - copies from src n chars to dest
+ * @dest: will be copied into
+ * @src: will be copied from
+ * @n: characters to copy
+ *
+ * Return: new dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
