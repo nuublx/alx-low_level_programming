3f34f3a0376eeb29cdd592097660a584fdc37eff
nuublx <zeyaddiaa21@gmail.com>
2023-03-01 05:23:44 -0800

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * string_toupper - converts all lowercase letters to uppercase letters
+ * @str: string to be converted
+ *
+ * Return: converted string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i ++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+			str[i] -= 32;
+	}
+	return (str);
+}
