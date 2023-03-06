15ff5918b9f815cc8c05681b092c41ce27bd6883
nuublx <zeyaddiaa21@gmail.com>
2023-03-06 06:46:44 -0800

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * _strstr - locates first occurence of a substring in a string
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: string starting with the substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i,j;
+
+	i = 0;
+	j = 0;
+	while (haystack[i] != '\0')
+	{
+		while (haystack[i] == needle[j])
+		{
+			i++;
+			j++;
+		}
+		if (j > 0 && needle[j - 1] == '\0')
+		{
+			return (haystack + i - j);
+		}
+		else
+			j = 0;
+		i++;
+	}
+	return (NULL);
+}
