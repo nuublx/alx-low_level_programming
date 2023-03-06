c1ba4ce78c054913ed47da30f989aad343de9e57
nuublx <zeyaddiaa21@gmail.com>
2023-03-06 04:00:31 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - returns a pointer to a position of a character in string
+ * @s: string passed to check inside of it
+ * @c: character to check for
+ *
+ * Return: a pointer to the first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	return (NULL);
+}
