567f9eabe511348000dfe925fb4137a3c42321a6
nuublx <zeyaddiaa21@gmail.com>
2023-03-08 03:26:04 -0800

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,13 @@
  */
 int _strlen_recursion(char *s)
 {
+	int count;
 	if (*s == '\0')
 		return (0);
 	else
 	{
 		s++;
-		int count = _strlen_recursion(s);
+		count = _strlen_recursion(s);
 		return (count + 1);
 	}
 }
