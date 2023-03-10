cd88cd7ce75eeb561530ead255672dd22fd84646
nuublx <zeyaddiaa21@gmail.com>
2023-03-10 02:56:33 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,9 +11,12 @@
 int main(int argc, char **argv)
 {
 	if (argc < 3)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	else
-		printf("%d", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }
