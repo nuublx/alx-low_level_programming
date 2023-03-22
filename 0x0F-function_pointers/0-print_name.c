26706a44e19065eef9c16f361c8785195a945b7f
nuublx <zeyaddiaa21@gmail.com>
2023-03-22 08:13:00 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name using another function
+ * @name: name to print
+ * @f: function to use to print name
+ *
+ * Return: Void ALWAYS
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
