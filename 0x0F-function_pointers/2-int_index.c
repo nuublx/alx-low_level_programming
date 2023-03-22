3fb8a66464144ee3f9d966f51193b7532029db83
nuublx <zeyaddiaa21@gmail.com>
2023-03-22 08:38:00 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * int_index -searches for an integer
+ * @array: array to search in
+ * @size: size of array
+ * @cmp: compartor to search with
+ *
+ * Return: -1 if no element matches or size < 1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 1 || !array || !cmp)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) > 0)
+			return (i);
+}
