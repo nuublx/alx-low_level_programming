dd9751e3d823129f381aae200dcd2c71a5cd139e
nuublx <zeyaddiaa21@gmail.com>
2023-03-23 08:27:14 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,11 +19,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
-		if (!separator)
+		if (!separator && i == 0)
 			printf("%d", va_arg(list, int));
+		else if (!separator)
+			printf(" %d", va_arg(list, int));
 		else if (separator && i == 0)
 			printf("%d", va_arg(list, int));
-		else 
+		else
 			printf("%s %d", separator, va_arg(list, int));
 	}
 	printf("\n");
